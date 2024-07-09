9099ac51a7a37dd62ed957a291f0b77a86f8c803
Ryan <90571282+issryan@users.noreply.github.com>
2024-07-09 17:25:02 -0400

    implemented simple navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Flick</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
